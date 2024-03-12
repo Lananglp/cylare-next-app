b7c1587635785df84ad2d1c5450100ebcc482896
lanang <lananglanusaputera@gmail.com>
2024-03-12 13:45:54 +0800

    lanang

--- src/app/components/layouts/menu/MultiMenu.js
+++ src/app/components/layouts/menu/MultiMenu.js
@@ -14,11 +14,15 @@ function MultiMenu(props) {
     useEffect(() => {
         const getHeight = dropdown.current && dropdown.current.scrollHeight;
         if (show) {
-            dropdown.current.classList.remove("max-h-0", "opacity-0");
-            dropdown.current.classList.add(`max-h-[${getHeight}px]`, "opacity-100");
+            dropdown.current.classList.remove("opacity-0");
+            dropdown.current.classList.add("opacity-100");
+
+            dropdown.current.style.maxHeight = getHeight.toString() + "px";
         } else {
-            dropdown.current.classList.remove(`max-h-[${getHeight}px]`, "opacity-100");
-            dropdown.current.classList.add("max-h-0", "opacity-0");
+            dropdown.current.classList.remove("opacity-100");
+            dropdown.current.classList.add("opacity-0");
+
+            dropdown.current.style.maxHeight = 0 + "px";
         }
     }, [show, dropdown]);
 
@@ -26,7 +30,7 @@ function MultiMenu(props) {
     <div className="ps-2">
         <button onClick={toggleShow} className="mb-2 flex justify-between items-center text-zinc-400 hover:text-white duration-500 w-full">Routing <i className={`${show && 'rotate-90'} fa fa-fw fa-sm fa-chevron-right duration-200`} /></button>
         <div className="overflow-hidden mt-2">
-            <div ref={dropdown} className="max-h-0 opacity-0 border-l border-zinc-800 ms-4 transition-all duration-500">
+            <div ref={dropdown} style={{maxHeight: '0px'}} className="opacity-0 border-l border-zinc-800 ms-4 transition-all duration-500">
                 <div className="flex flex-col gap-y-2 ps-2">
                     {children}
                 </div>
